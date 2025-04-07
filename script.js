efa6f93f2c339739179354bdc25067cbff7111d8
Ajeeta18 <163826179+Ajeeta18@users.noreply.github.com>
2025-04-07 08:43:57 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function showMessage1(text) {
+    window.location.href="daynight.html";
+}
+
+function showMessage2(text) {
+    window.location.href="map.html";
+}
+
+function showMessage3(text) {
+    window.location.href="tips.html";
+}
